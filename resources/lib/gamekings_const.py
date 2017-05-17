82f8ab547247372dedc607ca5224f9be39ba8011
skipmodea1 <skipmodea1@outlook.com>
2017-05-17 21:57:58 +0200

    v1.2.10 (2017-05-17)

--- resources/lib/gamekings_const.py
+++ resources/lib/gamekings_const.py
@@ -15,4 +15,4 @@ BASE_URL_GAMEKINGS_TV = "https://www.gamekings.tv/"
 LOGIN_URL = 'https://www.gamekings.tv/wp-login.php'
 TWITCH_URL =  'plugin://plugin.video.twitch/playLive/gamekings/'
 DATE = "2017-05-17"
-VERSION = "1.2.10-SNAPSHOT"
+VERSION = "1.2.10"
